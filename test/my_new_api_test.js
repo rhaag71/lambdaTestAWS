a3bdec44104a873905f5e6f35ea4d489fa75ce50
Rob Haag <rhaag@commandalkon.com>
2016-10-30 22:12:37 -0400

    Added another methoda and test

--- test/my_new_api_test.js
+++ test/my_new_api_test.js
@@ -3,18 +3,24 @@ var expect = require("chai").expect;
 var myNewApi = require("../lib/my_new_api.js");
 
 describe("myNewApi", function () { 
+
 	it("exports handleRequest", function () { 
 		assert.typeOf(myNewApi.handleRequest, "function"); 
 	});
 
 	
-	it("Sends back what you send to it", function (done) {
-
-		myNewApi.handleRequest({"Say":"hello"}, function(data1, data) {
-console.log(typeof(data));		
-console.log(data.Said);	
+	it("Should said world if you say hello", function (done) {
+		myNewApi.handleRequest({"Say":"hello"}, function(data1, data) {	
 			expect(data.Said).to.be.equal("World");
 			done();
 		});
 	});
+
+	it("Should send back temp if you send city", function (done) {
+		myNewApi.handleRequest({"temp":"columbus"}, function(data1, data) {
+			expect(data.colTemp).to.be.equal("65F");
+			done();
+		});
+	});
+	
 });
